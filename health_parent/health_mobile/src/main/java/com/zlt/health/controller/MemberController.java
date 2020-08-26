cc574a5029862803ca65b3d8bcdb85e815b859cd
zhanglt1995 <zhanglt1995@126.com>
2020-08-26 20:25:33 +0800

    security框架登录页面的美化

--- health_parent/health_mobile/src/main/java/com/zlt/health/controller/MemberController.java
+++ health_parent/health_mobile/src/main/java/com/zlt/health/controller/MemberController.java
@@ -7,10 +7,7 @@ import com.zlt.health.constant.RedisConstants;
 import com.zlt.health.pojo.Member;
 import com.zlt.health.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
@@ -34,7 +31,7 @@ public class MemberController {
     private MemberService memberService;
 
     @PostMapping("/login/check")
-    public Result check(@RequestParam("loginInfo") Map<String, Object> loginInfo, HttpServletResponse response) {
+    public Result check(@RequestBody Map loginInfo, HttpServletResponse response) {
         // 校验验证码是否是正确的
         Jedis jedis = jedisPool.getResource();
         // 获取前台传入的手机号和验证码
@@ -42,12 +39,12 @@ public class MemberController {
         String validateCode = (String) loginInfo.get("validateCode");
 
         // 获取redis缓存中的数据
-        String code = jedis.get(RedisConstants.SENDTYPE_LOGIN + telephone);
+        String code = jedis.get(RedisConstants.SENDTYPE_LOGIN + "_" + telephone);
         if (code == null) {
             return new Result(false, MessageConstants.TELEPHONE_VALIDATECODE_NOTNULL);
         }
 
-        if(!code.equals(validateCode)){
+        if (!code.equals(validateCode)) {
             return new Result(false, MessageConstants.VALIDATECODE_ERROR);
         }
         // 验证通过后删除缓存的验证码，防止重复验证
@@ -55,7 +52,7 @@ public class MemberController {
 
         // 通过手机号查询该用户是否存在，如果不存在，则注册
         Member member = memberService.findByTelephone(telephone);
-        if(null == member){
+        if (null == member) {
             // 会员不存在，添加为新会员
             member = new Member();
             member.setRegTime(new Date());
@@ -64,9 +61,9 @@ public class MemberController {
             memberService.add(member);
         }
         // 设置cookie信息，后面访问的可以获取到手机号
-        Cookie cookie = new Cookie(MessageConstants.LOGIN_TELEPHONE_COOKIE,telephone);
+        Cookie cookie = new Cookie(MessageConstants.LOGIN_TELEPHONE_COOKIE, telephone);
         // 存1个月
-        cookie.setMaxAge(30*24*60*60);
+        cookie.setMaxAge(30 * 24 * 60 * 60);
         // 访问的路径 根路径下时 网站的所有路径 都会发送这个cookie
         cookie.setPath("/");
         response.addCookie(cookie);
